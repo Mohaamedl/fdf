89adb32577fd3af20ff5ca3a497479b4339e7513
Mohamed <mohamedhadodo33@gmail.com>
2025-07-30 18:31:49 +0100

    init

---
+++ main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhaddadi <mhaddadi@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 18:46:07 by mhaddadi          #+#    #+#             */
+/*   Updated: 2025/07/28 18:46:53 by mhaddadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx/mlx.h"
+
+typedef struct s_mlx_data 
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img;
+	char	*addr;
+	int		line_len;
+	int		bits_per_pixel;
+	int		endian;
+}	t_mlx_data;
+
+void	ft_mlx_pixel_put(t_mlx_data *data, int x, int y, int color);
+
+int main(void)
+{
+	t_mlx_data	data;
+
+	data.mlx_ptr = mlx_init();
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 1920, 1080, "yoooo");
+	data.img = mlx_new_image(data.mlx_ptr, 1920, 1080);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_len, &data.endian);
+
+	ft_mlx_pixel_put(&data, 5, 5, 0x00FFFF00);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img, 0, 0);
+
+	mlx_loop(data.mlx_ptr);
+
+}
+
+
+void	ft_mlx_pixel_put(t_mlx_data *data, int x, int y, int color)
+{
+	char	*dest;
+	dest = data -> addr + (y * data -> line_len + x * (data -> bits_per_pixel / 8));
+	*(unsigned int *) dest = color;
+}
